dbbe336954b8ba4e8da055a789268883445941cb
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2020-08-06 12:34:20 -0300

    Estilizando o formulário do TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import './styles.css';
 import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 function TeacherForm(){
     return(
@@ -18,6 +19,29 @@ function TeacherForm(){
                 <Input name="avatar" label="Avatar"/>
                 <Input name="whatsapp" label="WhatsApp"/>
             </fieldset>
+
+            <fieldset>
+                <legend>Sobre a aula</legend>
+                <Input name="subject" label="Matéria"/>
+                <Input name="cost" label="Custo da sua hora por aula"/>
+            </fieldset>
+
+            <fieldset>
+                <legend>Seus dados</legend>
+                <Input name="name" label="Nome completo"/>
+                <Input name="avatar" label="Avatar"/>
+                <Input name="whatsapp" label="WhatsApp"/>
+            </fieldset>
+            <footer>
+                <p>
+                    <img src={warningIcon} alt="Aviso importante"/>
+                    Importante! <br />
+                    Preencha todos os dados
+                </p>
+                <button type="button">
+                    Salvar cadastro
+                </button>
+            </footer>
         </main>
     </div>
     )
