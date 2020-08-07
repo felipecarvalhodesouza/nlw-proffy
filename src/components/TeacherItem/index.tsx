e95dc9880037ac8234359c2e29e1f01ee7f6a56a
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2020-08-07 00:19:21 -0300

    Completando listagem de professores pelo endpoint, e integração com o chat do whatsapp no botão

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,30 +2,42 @@ import React from 'react';
 import './styles.css';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/36648569?s=460&u=d032d8d59d2e13ff16e5b4f5bed54fc4dd6dd3de&v=4" alt="Felipe Carvalho"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Felipe Carvalho de Souza</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entustiasta das melhores tecnologias de química avançada.
-                <br/><br/>
-                Apaixonado pro explodir coisas em laboratório e por mudar a vida das pessoas através de experiências [...]
-            </p>
+
+            <p>{teacher.bio}</p>
+
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article> 
     )
